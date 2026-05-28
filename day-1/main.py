8068e45c9e8f7ee18cead9be4ce07a09b4f1ca45
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-05-28 18:15:39 +0500

    day 1 update

--- day-1/main.py
+++ day-1/main.py
@@ -1,3 +1,4 @@
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -9,5 +10,27 @@ plt.title('Bar Chart')
 
 plt.xlabel('Day')
 plt.ylabel('Tip')
+
+plt.show()
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+
+data = pd.read_excel('data2.xlsx')
+
+data['Date'] = pd.to_datetime(data['Date']).dt.date
+
+
+counts = pd.crosstab(data['Date'], data['Product'])
+
+counts.plot(kind='bar', width=0.8)
+plt.title('Bar Chart')
+plt.xlabel('Date', fontsize=10)
+plt.ylabel('Product')
+
+plt.xticks(rotation=45)
 
 plt.show()
